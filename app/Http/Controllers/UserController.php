74e2d31f28d254a411afdfb0ae6ff27d140fcfd1
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-02-14 11:49:37 -0300

    stops projects in profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Completed;
 use App\Models\Favorite;
 use App\Models\Read;
+use App\Models\Stop;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,7 +21,9 @@ class UserController extends Controller
         $favorites = Favorite::where('user_id', Auth::user()->id)->with(['project'])->orderBy('updated_at', 'desc')->limit(10)->get();
         $completeds = Completed::where('user_id', Auth::user()->id)->with(['project'])->orderBy('updated_at', 'desc')->limit(10)->get();
         $reads = Read::where('user_id', Auth::user()->id)->with(['project'])->orderBy('updated_at', 'desc')->limit(10)->get();
+        $stops = Stop::where('user_id', Auth::user()->id)->with(['project'])->orderBy('updated_at', 'desc')->limit(10)->get();
 
-        return view('users.profile', compact('user', 'favorites', 'completeds', 'reads'));
+
+        return view('users.profile', compact('user', 'favorites', 'completeds', 'reads', 'stops'));
     }
 }
--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -198,6 +198,55 @@
 
     </section>
 
+    <section class="s-stops overflow-hidden  mt-[80px]">
+        <div class="container-title">
+            <div class="title">
+                <i class="material-symbols-outlined text-slate-500">
+                    lock
+                </i>
+                <h1>Parei</h1>
+            </div>
+            <form action="{{ route('save.index', ['type' => 'stops']) }}" method="GET" class="m-0">
+                <button class="btn w-[120px] p-1 rounded-[5px] bg-[#A93F3F] hover:bg-[#8A2D2D]">Ver mais</button>
+            </form>
+        </div>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                @foreach ($stops as $stop)
+                    <div class="swiper-slide">
+                        <div class="hidden">{{ $title = str_replace(' ', '-', $stop->project->title) }}</div>
+                        <div class="card-manga">
+                            <div class="img">
+                                <div class="type">
+                                    <img src="
+                    @if ($stop->project->type == 'Manwha') {{ asset('img/type-manwha.png') }}
+                    @elseif($stop->project->type == 'Novel')
+                        {{ asset('img/type-novel.png') }}
+                    @elseif($stop->project->type == 'Manga')
+                        {{ asset('img/type-manga.png') }} @endif
+                "
+                                        alt="">
+                                </div>
+
+                                <a href="{{ route('project.show', ['id' => $stop->project->id]) }}">
+                                    <img class="photo-manga"
+                                        src="{{ asset("projects/$title/banner/" . $stop->project->banner) }}"
+                                        alt="">
+                                </a>
+                            </div>
+                            <div class="title">
+                                <p>{{ $stop->project->title }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+
+
+    </section>
+
     
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>
