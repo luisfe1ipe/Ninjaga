e7460d5729a4f68579802230fb15d1c27e5b8e50
Luis Felipe <luisfelipe.pxl@gmail.com>
2023-02-19 19:00:35 -0300

    save projects with livewire

--- app/Http/Controllers/Project/SaveController.php
+++ app/Http/Controllers/Project/SaveController.php
@@ -8,7 +8,6 @@ use App\Models\Favorite;
 use App\Models\Project;
 use App\Models\Read;
 use App\Models\Stop;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SaveController extends Controller
@@ -34,75 +33,5 @@ class SaveController extends Controller
             $stops = Stop::where('user_id', Auth::user()->id)->orderBy('updated_at', 'desc')->get();
             return view('save.index', compact('stops'));
         }
-
     }
-
-
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        $project = Project::find($data['project_id']);
-        
-        $userId = Auth::user()->id;
-
-        
-        $projectFav = Favorite::where('project_id', $project->id)->where('user_id', $userId)->exists();
-        $projectComp = Completed::where('project_id', $project->id)->where('user_id', $userId)->exists();
-        $projectRead = Read::where('project_id', $project->id)->where('user_id', $userId)->exists();
-        $projectStop = Stop::where('project_id', $project->id)->where('user_id', $userId)->exists();
-
-
-        if ($data['save'] == 'read' or $data['save'] == 'stop' or $data['save'] == 'favorite' or $data['save'] == 'completed') {
-            if ($projectFav) {
-                Favorite::where('project_id', $project->id)->where('user_id', Auth::user()->id)->delete();
-            }
-
-            if ($projectComp) {
-                Completed::where('project_id', $project->id)->where('user_id', Auth::user()->id)->delete();
-            }
-
-            if ($projectRead) {
-                Read::where('project_id', $project->id)->where('user_id', Auth::user()->id)->delete();
-            }
-
-            if ($projectStop) {
-                Stop::where('project_id', $project->id)->where('user_id', Auth::user()->id)->delete();
-            }
-
-            if ($data['save'] == 'read') {
-                if ($projectRead) {
-                    Read::where('project_id', $project->id)->where('user_id', $userId)->delete();
-                    return back()->with('deleted', "$project->title removido dos lerei.");
-                } else {
-                    $project->read()->create($data);
-                    return back()->with('success', "$project->title adicionado aos lerei.");
-                }
-            } else if ($data['save'] == 'stop') {
-                if ($projectStop) {
-                    Stop::where('project_id', $project->id)->where('user_id', $userId)->delete();
-                    return back()->with('deleted', "$project->title removido dos parei.");
-                } else {
-                    $project->stop()->create($data);
-                    return back()->with('success', "$project->title adicionado aos parei.");
-                }
-            } else if ($data['save'] == 'completed') {
-                if ($projectComp) {
-                    Completed::where('project_id', $project->id)->where('user_id', $userId)->delete();
-                    return redirect()->back()->with('deleted', "$project->title removido dos lidos.");
-                } else {
-                    $project->completed()->create($data);
-                    return back()->with('success', "$project->title adicionado aos lidos.");
-                }
-            } else if ($data['save'] == 'favorite') {
-                if ($projectFav) {
-                    Favorite::where('project_id', $project->id)->where('user_id', Auth::user()->id)->delete();
-                    return back()->with('deleted', "$project->title removido dos favoritos.");
-                } else {
-                    $project->favorite()->create($data);
-                    return back()->with('success', "$project->title adicionado aos favoritos.");
-                }
-            }
-        }
-    }
-
 }
