5ab421526ecb9e9d6de6200a3e3064817e4f1697
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-05-29 16:13:32 -0300

    criando factory genres

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Studio::factory(50)->create();
         \App\Models\Type::factory(5)->create();
         \App\Models\Status::factory(3)->create();
+        \App\Models\Genre::factory(25)->create();
 
     }
 }
