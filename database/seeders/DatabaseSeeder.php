319f6ccb423715c4035e26c764f255d26f6bff5f
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-05-29 15:26:01 -0300

    criando factory studio

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Author::factory(50)->create();
+        // \App\Models\Author::factory(50)->create();
+        \App\Models\Studio::factory(50)->create();
     }
 }
