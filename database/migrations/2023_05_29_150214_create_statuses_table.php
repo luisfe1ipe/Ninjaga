db809e39c85616518488b1517d897bed0fda09f0
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-05-29 20:01:05 -0300

    criando status na migration

--- database/migrations/2023_05_29_150214_create_statuses_table.php
+++ database/migrations/2023_05_29_150214_create_statuses_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,16 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        Status::create([
+            'name' => 'Lançamento'
+        ]);
+        Status::create([
+            'name' => 'Hiato'
+        ]);
+        Status::create([
+            'name' => 'Dropado'
+        ]);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Author::factory(50)->create();
         \App\Models\Studio::factory(50)->create();
         \App\Models\Type::factory(5)->create();
-        \App\Models\Status::factory(3)->create();
+        // \App\Models\Status::factory(3)->create();
         \App\Models\Genre::factory(25)->create();
         \App\Models\Project::factory(150)->create();
     }
