db4c1162d659d89ce3ed493bbe90795b67a1b501
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-05-29 14:31:25 -0300

    crud projects sem relacionamentos

---
+++ app/Filament/Resources/ProjectResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProjectResource\Pages;
+use App\Filament\Resources\ProjectResource\RelationManagers;
+use App\Models\Project;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProjectResource extends Resource
+{
+    protected static ?string $model = Project::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('banner')
+                    ->required()
+                    ->columnSpan(2)
+                    ->imagePreviewHeight('250'),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('released_year')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\MarkdownEditor::make('synopsis')
+                    ->required()
+                    ->maxLength(65535)
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('banner'),
+                Tables\Columns\TextColumn::make('title')
+                    ->limit(25),
+                Tables\Columns\TextColumn::make('synopsis')
+                    ->limit(45),
+                Tables\Columns\TextColumn::make('released_year'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime('d/m/Y')->since(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime('d/m/Y')->since(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjects::route('/'),
+            'create' => Pages\CreateProject::route('/create'),
+            'edit' => Pages\EditProject::route('/{record}/edit'),
+        ];
+    }
+}
